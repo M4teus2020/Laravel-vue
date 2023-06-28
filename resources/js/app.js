447898e967e663411d64544178c806a387c9a0cb
M4teus2020 <mfelini7@gmail.com>
2023-06-27 22:07:12 -0300

    Laravel and vue aplication tutorial part 3

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+import './bootstrap';
+
+import 'admin-lte/plugins/jquery/jquery.js'
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.js'
+import 'admin-lte/dist/js/adminlte.js'
+
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import Routes from './routes';
+
+const app = createApp({});
+
+const routes = createRouter({
+    routes: Routes,
+    history: createWebHistory(),
+})
+
+app.use(routes);
+
+app.mount('#App');
